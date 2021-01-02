9c48b2490740cb1e413139ef5c94e5e963e492a2
dbydd <1992003927@qq.com>
2021-01-02 11:20:53 +0800

    enable OBJLoader

--- src/main/java/com/birkwald/maschinenbaucraft/utils/handler/RegistryHandler.java
+++ src/main/java/com/birkwald/maschinenbaucraft/utils/handler/RegistryHandler.java
@@ -1,7 +1,10 @@
 package com.birkwald.maschinenbaucraft.utils.handler;
 
+import com.birkwald.maschinenbaucraft.MaschinenBau_Craft;
 import com.birkwald.maschinenbaucraft.registered_lists.Items;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -16,6 +19,11 @@ public class RegistryHandler {
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(Items.ITEMS.values().toArray(new Item[0]));
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        OBJLoader.INSTANCE.addDomain(MaschinenBau_Craft.MODID);
     }
 
     public static void preInitRegistries(FMLPreInitializationEvent event) {
